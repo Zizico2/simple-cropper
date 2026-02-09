7d7e48a3c4b0d16a5e2ded0f0f1514baee5bab9f
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2026-02-09 17:30:26 +0000

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
 
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>Accurate Image Cropper</h1>
+      <h1 className={styles.title}>Image Cropper</h1>
 
       {!imageSrc ? (
         <ImageUpload onImageSelected={setImageSrc} />
