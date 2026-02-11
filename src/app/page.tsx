2c90bac5a77287e3e1af39a84703d331f1178bfa
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2026-02-11 01:04:34 +0000

    make RSC's async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import CropperApp from "../components/CropperApp";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className={styles.container}>
       <h1 className={styles.title}>Simple Cropper</h1>
